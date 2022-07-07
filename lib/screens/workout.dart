d17dc5b258b10f967deb0a0928e7805b134c7ee9
sakshipore <88977771+sakshipore@users.noreply.github.com>
2022-07-07 20:34:15 +0530

    pause screen added

--- lib/screens/workout.dart
+++ lib/screens/workout.dart
@@ -13,118 +13,203 @@ class WorkOut extends StatelessWidget {
     return ChangeNotifierProvider<TimerModelSec>(
       create: (context) => TimerModelSec(context),
       child: Scaffold(
-          body: Column(
-        children: [
-          Container(
-            height: 300,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image: NetworkImage(
-                    "https://images.unsplash.com/photo-1544367567-0f2fcb009e0b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=920&q=80"),
-              ),
-            ),
-          ),
-          Spacer(),
-          Text(
-            "Anulom Vilom",
-            style: TextStyle(
-              fontSize: 40,
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-          SizedBox(
-            height: 30
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 80),
-            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-            decoration: BoxDecoration(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.circular(50)),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "00",
-                  style: TextStyle(
-                    fontSize: 25,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
+        body: Stack(
+          children: [
+            Container(
+              child: Column(
+                children: [
+                  Container(
+                    height: 300,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                            "https://images.unsplash.com/photo-1544367567-0f2fcb009e0b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=920&q=80"),
+                      ),
+                    ),
                   ),
-                ),
-                Text(
-                  " : ",
-                  style: TextStyle(
-                    fontSize: 25,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
+                  Spacer(),
+                  Text(
+                    "Anulom Vilom",
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.w600,
+                    ),
                   ),
-                ),
-                Consumer<TimerModelSec>(
-                  builder: (context, myModel, child) {
-                    return Text(
-                      myModel.countdown.toString(),
-                      style: TextStyle(
-                        fontSize: 25,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
+                  SizedBox(height: 30),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 80),
+                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                    decoration: BoxDecoration(
+                        color: Colors.blueAccent,
+                        borderRadius: BorderRadius.circular(50)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "00",
+                          style: TextStyle(
+                            fontSize: 25,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          " : ",
+                          style: TextStyle(
+                            fontSize: 25,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Consumer<TimerModelSec>(
+                          builder: (context, myModel, child) {
+                            return Text(
+                              myModel.countdown.toString(),
+                              style: TextStyle(
+                                fontSize: 25,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            );
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 60,
+                  ),
+                  Consumer<TimerModelSec>(builder: (context, myModel, child) {
+                    return ElevatedButton(
+                      onPressed: () {
+                        myModel.show();
+                      },
+                      child: Container(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                        child: Text(
+                          "PAUSE",
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
                       ),
                     );
-                  },
-                ),
-              ],
-            ),
-          ),
-          SizedBox(
-            height: 60,
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: Container(
-              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-              child: Text(
-                "PAUSE",
-                style: TextStyle(
-                  fontSize: 20,
-                ),
+                  }),
+                  Spacer(),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 15),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            "PREVIOUS",
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ),
+                        TextButton(
+                            onPressed: () {},
+                            child: Text("NEXT", style: TextStyle(fontSize: 16)))
+                      ],
+                    ),
+                  ),
+                  Divider(
+                    thickness: 2,
+                    endIndent: 15,
+                    indent: 15,
+                  ),
+                  Align(
+                    alignment: Alignment.bottomLeft,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 15),
+                      child: Text("Next: Anulom Vilom",
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold)),
+                    ),
+                  ),
+                ],
               ),
             ),
-          ),
-          Spacer(),
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 15),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    "PREVIOUS",
-                    style: TextStyle(fontSize: 16),
+            Consumer<TimerModelSec>(builder: (context, myModel, child) {
+              return Visibility(
+                visible: myModel.visible,
+                child: Container(
+                  color: Colors.blueAccent.withOpacity(0.9),
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "PAUSE",
+                        style: TextStyle(
+                            fontSize: 40,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "Yoga feels better",
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.white,
+                        ),
+                      ),
+                      OutlinedButton(
+                        onPressed: () {},
+                        child: Container(
+                          width: 180,
+                          child: Text(
+                            "RESTART",
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.white)),
+                      ),
+                      OutlinedButton(
+                        onPressed: () {},
+                        child: Container(
+                          width: 180,
+                          child: Text(
+                            "QUIT",
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.white)),
+                      ),
+                      OutlinedButton(
+                        onPressed: () {
+                          myModel.hide();
+                        },
+                        child: Container(
+                          width: 180,
+                          child: Text(
+                            "RESUME",
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.white)),
+                      ),
+                    ],
                   ),
                 ),
-                TextButton(
-                    onPressed: () {},
-                    child: Text("NEXT", style: TextStyle(fontSize: 16)))
-              ],
-            ),
-          ),
-          Divider(
-            thickness: 2,
-            endIndent: 15,
-            indent: 15,
-          ),
-          Align(
-            alignment: Alignment.bottomLeft,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-              child: Text("Next: Anulom Vilom",
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            ),
-          ),
-        ],
-      )),
+              );
+            }),
+          ],
+        ),
+      ),
     );
   }
 }
@@ -134,6 +219,7 @@ class TimerModelSec with ChangeNotifier {
     MyTimerSec(context);
   }
   int countdown = 30;
+  bool visible = false;
 
   MyTimerSec(context) async {
     Timer.periodic(Duration(seconds: 1), (timer) {
@@ -145,5 +231,15 @@ class TimerModelSec with ChangeNotifier {
             context, MaterialPageRoute(builder: (context) => BreakTime()));
       }
     });
+  }
+
+  void show() {
+    visible = true;
+    notifyListeners();
+  }
+
+  void hide() {
+    visible = false;
+    notifyListeners();
   }
 }
